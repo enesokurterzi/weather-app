5a8bd4941ea02b666041646c69fd044d165a0315
Enes OKURTERZİ <enes.okurterzi98@gmail.com>
2025-05-18 13:43:12 +0300

    add edge to edge and predictive back

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
@@ -13,11 +14,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,52 +46,65 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         permissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) {
             viewModel.loadInfo()
         }
-        permissionLauncher.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ))
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
         setContent {
             WeatherAppTheme {
-                Box(modifier = Modifier.fillMaxSize()
-                    .background(DarkBlue)) {
-                    if (!viewModel.state.isLoading) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .background(DarkBlue)
-                                .verticalScroll(rememberScrollState())
-                        ) {
-                            WeatherCard(
-                                state = viewModel.state,
-                                backgroundColor = DeepBlue
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            WeatherForecast(state = viewModel.state)
-                            Spacer(modifier = Modifier.height(16.dp))
-                            DailyWeatherForecast(state = viewModel.state)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets.safeDrawing
+                ) { paddingValues ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(DarkBlue)
+                    ) {
+                        if (!viewModel.state.isLoading) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(paddingValues)
+                                    .background(DarkBlue)
+                                    .verticalScroll(rememberScrollState())
+                            ) {
+                                WeatherCard(
+                                    state = viewModel.state,
+                                    backgroundColor = DeepBlue
+                                )
+                                Spacer(modifier = Modifier.height(16.dp))
+                                WeatherForecast(state = viewModel.state)
+                                Spacer(modifier = Modifier.height(16.dp))
+                                DailyWeatherForecast(state = viewModel.state)
+
+                            }
 
                         }
-
-                    }
-                    if (viewModel.state.isLoading) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                    viewModel.state.error?.let { error ->
-                        Text(
-                            text = error,
-                            color = Color.Red,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
+                        if (viewModel.state.isLoading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                        viewModel.state.error?.let { error ->
+                            Text(
+                                text = error,
+                                color = Color.Red,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
                     }
                 }
+
             }
         }
     }
